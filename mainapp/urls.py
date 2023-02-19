c6661b8da510a7e4a74e770592eab09ad888fe36
DimaFX <dm.mokrousoff@yandex.ru>
2023-02-20 01:58:33 +0300

    lesson_5 Authapp done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,6 +5,13 @@ from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name
 
+from django.urls import path
+
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
+
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
@@ -17,5 +24,4 @@ urlpatterns = [
     ),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
-    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
